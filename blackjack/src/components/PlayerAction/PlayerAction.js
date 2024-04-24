f6dca80d11f69516ef7b60b6a811daf4977cd011
Jia Li <114707005+jiali1118@users.noreply.github.com>
2024-04-24 15:28:10 -0700

    added playerAction component

---
+++ blackjack/src/components/PlayerAction/PlayerAction.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function playerAction() {
+  return (
+    <div>
+      <button>Hit</button>
+      <button>Double</button>
+      <button>Stand</button>
+      <button>Split</button>
+    </div>
+  );
+}
+
+export default playerAction;
