731f4d1cd390b32c3b74a54d3e469aac00808c34
Jia Li <114707005+jiali1118@users.noreply.github.com>
2024-05-20 16:45:05 -0700

    removed unused code

--- blackjack/src/components/BetAmount/BetAmount.js
+++ blackjack/src/components/BetAmount/BetAmount.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const BetAmount = ({ state, dispatch }) => {
   const [totalBetAmount, setTotalBetAmount] = useState(0);
--- blackjack/src/components/Home/Home.js
+++ blackjack/src/components/Home/Home.js
@@ -1,24 +1,11 @@
 import React, { useState } from "react";
 import BlackjackGame from "../BlackJack/BlackJack";
 import Header from "../Header/header";
-import LeaderboardModal from "../Leaderboard/LeaderboardModal";
-import { Button } from "react-bootstrap";
 
-function Home(inputEmail) {
-  //Leaderboard logic
-  const [showModal, setShowModal] = useState(false);
-  const handleShow = () => {
-    setShowModal(true);
-    console.log("Modal clicked");
-  };
-  const handleClose = () => setShowModal(false);
+function Home() {
   return (
     <div className="App">
       <Header />
-      <Button variant="primary" onClick={handleShow}>
-        Leaderboard
-      </Button>
-      <LeaderboardModal show={showModal} handleClose={handleClose} />
       <BlackjackGame />
     </div>
   );
