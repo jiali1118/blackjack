09d51db5d7903ab34e0b1f2eb1f762f7f194024e
Jia Li <114707005+jiali1118@users.noreply.github.com>
2025-02-09 10:56:49 -0800

    remove leaderboard H2, redundant

--- blackjack/src/components/Leaderboard/Leaderboard.js
+++ blackjack/src/components/Leaderboard/Leaderboard.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
-const Leaderboard = ({users}) => {
-  return(
-<div>
-  <h2>Leaderboard</h2>
-  <ol>
-    {users.map((user) =>(
-      <li key={user.id}>
-       {user.email} : ${user.highest_balance}
-      </li>
-    ))}
-  </ol>
-</div>
-  )
+const Leaderboard = ({ users }) => {
+  return (
+    <div>
+      <ol>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.email} : ${user.highest_balance}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default Leaderboard;
